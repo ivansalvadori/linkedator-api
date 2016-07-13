678d258973f3a3c0e3bdddd9e3a1fe61d890bdd3
ivan <ivanlsalvadori@gmail.com>
2016-07-12 21:23:49 -0300

    Minor Fixes

--- src/main/java/br/ufsc/inf/lapesd/linkedator/api/endpoint/SemanticMicroserviceDescriptionEndpoint.java
+++ src/main/java/br/ufsc/inf/lapesd/linkedator/api/endpoint/SemanticMicroserviceDescriptionEndpoint.java
@@ -1,17 +1,54 @@
 package br.ufsc.inf.lapesd.linkedator.api.endpoint;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+import javax.annotation.PostConstruct;
+import javax.ws.rs.Consumes;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import br.ufsc.inf.lapesd.linkedator.Linkedator;
 import br.ufsc.inf.lapesd.linkedator.SemanticMicroserviceDescription;
 
-@Path("microservice/description")
+@Component
+@Path("/")
 public class SemanticMicroserviceDescriptionEndpoint {
 
+    @Context 
+    javax.servlet.http.HttpServletRequest request; 
+
+
+    @Value("${config.ontologyFilePath}")
+    private String ontologyFilePath;
+    private Linkedator linkedator = null;
+
+    @PostConstruct
+    public void init() throws IOException {
+        String ontology = new String(Files.readAllBytes(Paths.get(ontologyFilePath)));
+        this.linkedator = new Linkedator(ontology);
+    }
+
     @POST
+    @Path("microservice/description")
+    @Consumes(MediaType.APPLICATION_JSON)
     public Response registryMicroserviceDescription(SemanticMicroserviceDescription semanticMicroserviceDescription) {
-        return null;
+        linkedator.registryDescription(semanticMicroserviceDescription);
+        return Response.ok().build();
+    }
+
+    @POST
+    @Path("createLinks")
+    public Response registryMicroserviceDescription(String resourceRepresentation) {
+        String representationWithLinks = linkedator.createLinks(resourceRepresentation);
+        return Response.ok(representationWithLinks).build();
     }
 
 }
