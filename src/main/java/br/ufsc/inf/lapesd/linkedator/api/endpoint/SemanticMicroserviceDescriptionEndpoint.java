85ff9d988bd10e1fed0ad2aef987ce469bb5cb82
ivan <ivanlsalvadori@gmail.com>
2017-03-08 14:33:04 -0300

    Interation with alignator for heterogeneous ontology alignament. Creation of a merged ontology to allows the link creation among heterogeneus ontology.

--- src/main/java/br/ufsc/inf/lapesd/linkedator/api/endpoint/SemanticMicroserviceDescriptionEndpoint.java
+++ src/main/java/br/ufsc/inf/lapesd/linkedator/api/endpoint/SemanticMicroserviceDescriptionEndpoint.java
@@ -1,20 +1,26 @@
 package br.ufsc.inf.lapesd.linkedator.api.endpoint;
 
 import java.io.IOException;
+import java.io.StringWriter;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.Base64;
+import java.util.List;
 
 import javax.annotation.PostConstruct;
 import javax.servlet.http.HttpServletRequest;
 import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
+import org.apache.jena.ontology.OntModel;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
@@ -44,6 +50,8 @@ public class SemanticMicroserviceDescriptionEndpoint {
     @Value("${config.cacheExpireAfterAccessSeconds}")
     private int cacheExpireAfterAccessSeconds;
 
+    private List<SemanticMicroserviceDescription> semanticMicroserviceDescriptions = new ArrayList<>();
+
     private Linkedator linkedator = null;
 
     private Alignator alignator = null;
@@ -55,6 +63,7 @@ public class SemanticMicroserviceDescriptionEndpoint {
         this.linkedator.enableCache(enableCache);
         this.linkedator.setCacheConfiguration(cacheMaximumSize, cacheExpireAfterAccessSeconds);
         alignator = new Alignator();
+
     }
 
     @POST
@@ -68,15 +77,52 @@ public class SemanticMicroserviceDescriptionEndpoint {
         String ontologyBase64 = semanticMicroserviceDescription.getOntologyBase64();
         String ontology = new String(Base64.getDecoder().decode(ontologyBase64.getBytes()));
         alignator.registerService(new Gson().fromJson(semanticMicroserviceDescription.toString(), ServiceDescription.class), ontology);
+
+        this.semanticMicroserviceDescriptions.add(semanticMicroserviceDescription);
+
         return Response.ok().build();
     }
 
     @POST
     @Path("createLinks")
-    public Response registryMicroserviceDescription(String resourceRepresentation, @QueryParam("verifyLinks") boolean verifyLinks) {
-        String representationWithLinks = linkedator.createLinks(resourceRepresentation, verifyLinks);
-        alignator.loadEntitiesAndAlignOntologies(representationWithLinks);
-        return Response.ok(representationWithLinks).build();
+    @SuppressWarnings("finally")
+    public Response createLins(String resourceRepresentation, @QueryParam("verifyLinks") boolean verifyLinks) {
+
+        try {
+            String ontology = new String(Files.readAllBytes(Paths.get("alignator-merged-ontology.owl")));
+            this.linkedator = new Linkedator(ontology);
+            this.linkedator.enableCache(enableCache);
+            this.linkedator.setCacheConfiguration(cacheMaximumSize, cacheExpireAfterAccessSeconds);
+
+            for (SemanticMicroserviceDescription semanticMicroserviceDescription : semanticMicroserviceDescriptions) {
+                this.linkedator.registryMicroserviceDescription(semanticMicroserviceDescription);
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            final String representationWithLinks = linkedator.createLinks(resourceRepresentation, verifyLinks);
+            alignator.loadEntitiesAndAlignOntologies(representationWithLinks);
+            return Response.ok(representationWithLinks).build();
+        }
     }
 
+    @GET
+    @Path("ontology")
+    @Produces(MediaType.APPLICATION_XML)
+    public Response loadOntology() {
+
+        OntModel ontoModel = this.linkedator.getOntologyReader().getOntoModel();
+
+        String ontology = "";
+        try (StringWriter out = new StringWriter()) {
+            ontoModel.write(out, "RDF/XML");
+            ontology = out.toString();
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+
+        return Response.ok(ontology).build();
+
+    }
 }
